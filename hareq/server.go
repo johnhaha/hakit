668455dda0a52ef389400600ebf15497ddd774c0
John Wu <11442142+johnhaha@users.noreply.github.com>
2021-10-13 08:01:29 +0800

    lazy commit: update req

--- hareq/server.go
+++ hareq/server.go
@@ -11,3 +11,25 @@ func BodyParser(r *http.Request, body interface{}) error {
 	err := decoder.Decode(body)
 	return err
 }
+
+//http send json
+func SendJson(w http.ResponseWriter, data interface{}) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(js)
+	return err
+}
+
+//http send string
+func SendString(w http.ResponseWriter, data string) error {
+	_, err := w.Write([]byte(data))
+	return err
+}
+
+//http set header
+func SetStatus(w http.ResponseWriter, status int) {
+	w.WriteHeader(status)
+}
