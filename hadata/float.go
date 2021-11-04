172680ed9741b073a22bdfc7299f811b0c6a12bb
John Wu <11442142+johnhaha@users.noreply.github.com>
2021-11-04 13:41:24 +0800

    get string from float

--- hadata/float.go
+++ hadata/float.go
@@ -1,8 +1,15 @@
 package hadata
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
 func GetFloatFromString(d string) (float64, error) {
 	f, err := strconv.ParseFloat(d, 64)
 	return f, err
+}
+
+func GetStringFromFloat(f float64) string {
+	return fmt.Sprintf("%f", f)
 }
