e1b57cbf19498b69fa5ce0d1ac12f82e495c1661
john <wujun.okok@gmail.com>
2022-01-31 08:48:17 +0800

    add interface slice check

--- hadata/inderface.go
+++ hadata/inderface.go
@@ -1,5 +1,7 @@
 package hadata
 
+import "reflect"
+
 func GetStringFromInterface(x interface{}) string {
 	switch v := x.(type) {
 	case string:
@@ -7,4 +9,8 @@ func GetStringFromInterface(x interface{}) string {
 	default:
 		return ""
 	}
+}
+
+func CheckInterfaceIsSlice(x interface{}) bool {
+	return reflect.TypeOf(x).Kind() == reflect.Slice
 }
