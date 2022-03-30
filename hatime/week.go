22d7bf3308d32348dd3cd095ca4c6e8b158c2f3f
John Wu <wujun.okok@gmail.com>
2022-03-30 18:19:18 +0800

    modify day

--- hatime/week.go
+++ hatime/week.go
@@ -6,19 +6,26 @@ import (
 	"github.com/jinzhu/now"
 )
 
+// caution: sunday is last day of week
 func SaturdayFrom(tm time.Time) time.Time {
+	now.WeekStartDay = time.Monday
 	return now.With(tm).Sunday().Add(-time.Hour * 24)
 }
 
+// caution: sunday is last day of week
 func SundayFrom(tm time.Time) time.Time {
+	now.WeekStartDay = time.Monday
 	return now.With(tm).Sunday()
 }
 
+// caution: sunday is last day of week
 func SaturdayOf(tm time.Time) time.Time {
-	return now.EndOfWeek().Add(-time.Hour*24 + 1)
+	now.WeekStartDay = time.Monday
+	return now.With(tm).EndOfWeek().Add(-time.Hour*24 + 1)
 }
 
 // caution: sunday is last day of week
 func SundayOf(tm time.Time) time.Time {
-	return now.EndOfWeek().Add(1)
+	now.WeekStartDay = time.Monday
+	return now.With(tm).EndOfWeek().Add(1)
 }
